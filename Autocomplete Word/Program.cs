a5cac6fee7d65bf5c62e5ea8df8ab42cdbdbb2ff
Станислав Шабанов <marazm56@gmail.com>
2021-07-04 15:02:58 +0700

    Add files via upload

---
+++ Autocomplete Word/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Autocomplete_Word.FindWord;
+
+namespace Autocomplete_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataExel dataExel = new DataExel();
+            var data =  dataExel.ReadExeleName();
+
+            DataInsertToWord dataInsertToWord = new DataInsertToWord(data);
+            dataInsertToWord.FindFileAndCreate();
+            dataInsertToWord.FindWord();
+        }
+    }
+}
